b7c204ac328c4c7bf8bf584b1c3781bca7fdf6d2
profudevops <60400533+profudevops@users.noreply.github.com>
2021-07-26 12:05:01 -0500

    Update ultimaversion.groovy

--- vars/ultimaversion.groovy
+++ vars/ultimaversion.groovy
@@ -1,3 +1,48 @@
-def call(String name = '$name') {
- echo "Welcome libreria, ${name}."
+def call() {
+ echo "#########GENERERA JSON COMPLETO#########"
+					 withCredentials([usernamePassword(credentialsId: 'CREDENCIALES_NEXUS-PROD', passwordVariable: 'PWD_NEXUS', usernameVariable: 'USER_NEXUS')]) {
+						  response= sh ( script: " curl -u ${USER_NEXUS}:${PWD_NEXUS} -X GET "+
+						  "http://172.22.50.14:8081/nexus/service/rest/v1/search/assets?repository=$NAMEREPO -H "+
+						  "'accept: application/json' ", returnStdout: true)
+						  props = readJSON text:  "$response"
+						  def propstoken = props.continuationToken
+						  props = props.items.path
+						  writeFile file:'json1', text: "$props"
+						  def count = 1
+							  while("$propstoken" != "null"){
+								       echo "####### ENTRA AL WHILE ########"
+                response= sh ( script: ' curl -u ${USER_NEXUS}:${PWD_NEXUS} -X GET '+
+                "'http://172.22.50.14:8081/nexus/service/rest/v1/search/assets?continuationToken="+
+                "$propstoken"+"&repository=$NAMEREPO'"+" -H 'accept: application/json' ", returnStdout: true)
+								        props = readJSON text:  "$response"
+								        propstoken = props.continuationToken
+								        echo "$propstoken"
+								        count++;
+								        echo "$count";
+								        props = props.items.path
+								        writeFile file: 'json'+"$count", text: "$props"
+							  }
+						  sh "cat json* > json"
+					}
+					
+					echo "#########LIMPIA JSON COMPLETO#########"
+					withCredentials([usernamePassword(credentialsId: 'CREDENCIALES_NEXUS-QA', passwordVariable: 'PWD_NEXUS', usernameVariable: 'USER_NEXUS')]) {
+						sh label: "shell para limpiar json", script: "curl -X GET -u ${USER_NEXUS}:${PWD_NEXUS} "+
+						"http://172.22.178.4:8081/nexus/repository/thirdparty/sh/limpiarjson/1.0/limpiarjson-1.0.sh -O"
+						sh label:"ultima version", script: "sh limpiarjson-1.0.sh json ${DIRECTORYNEXUS}$ProjectName/temporal/"
+						uversion = sh (script: 'cat UVERSION.txt', returnStdout: true )
+						echo "valor actual: $uversion"
+						if(uversion == "")
+						{
+						    echo "vacio el valor"
+						    uversion = 0
+						    uversion = uversion+1
+						    echo "valor de esta construccion: $uversion"
+						}else{
+						    echo "si tiene valor"
+						    uversion = uversion.toInteger()
+						    uversion = uversion+1
+						    echo "valor final: $uversion"
+						}
+					}
 }
