b63c211ddf964eb5f04ef16ff78f326368f85819
profudevops <60400533+profudevops@users.noreply.github.com>
2021-07-26 17:41:56 -0500

    Update prueba.groovy

--- vars/prueba.groovy
+++ vars/prueba.groovy
@@ -1,5 +1,6 @@
-def call(String name = '$name') {
+def call(String name = '$name', String ap = '$ap' ) {
  echo "Welcome, ${name}."
+ echo "apellido, ${ap}"
  name = "alejandro"
  echo "buenvenido $name"
  return name
