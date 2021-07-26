23a38a46de7a4f5ac6fe7a64e35603c0071ff734
profudevops <60400533+profudevops@users.noreply.github.com>
2021-07-26 17:51:44 -0500

    Update prueba.groovy

--- vars/prueba.groovy
+++ vars/prueba.groovy
@@ -3,5 +3,5 @@ def call(String name = '$name', String ap = '$apellido' ) {
  echo "este es el segundo parametro, ${ap}"
  name = "alejandro"
  echo "buenvenido $name"
- //return name
+ return name
 }
